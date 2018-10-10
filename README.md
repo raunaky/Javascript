d02a2f4b19282616af7c8ffa067e69bb9215c84a
Raunak Yadav <raunakyadav@Raunaks-MacBook-Air.local>
2018-10-09 23:55:56 -0400

    Removed old stuff

--- README.md
+++
@@ -1,3 +0,0 @@
-# Javascript
-We are using pluralsight boilerplate for this particular stack creation. 
-Dated : 08/04/2017 @ 9:02 pm Bangalore India.
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import numeral from 'numeral';
-import chalk from 'chalk';
-import css from './index.css';
-
-const coursevalue = numeral(1000).format('$0,0.00');
-console.log(chalk.bgRed(`I would pay ${coursevalue} for this awsome course`));
